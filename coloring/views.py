fc30a8cfac336c5e9e8c8bf75c19b598a983d591
Claire Yang <claire.yang@berkeley.edussh>
2021-07-12 18:17:40 -0400

    new files

--- coloring/views.py
+++ coloring/views.py
@@ -2,3 +2,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'coloring/index.html')
+
+def rec_easy(request):
+    return render(request, 't2/rec_easy.html')
+
+def rec_hard(request):
+    return render(request, 't2/rec_hard.html')
+
+def color_easy(request):
+    return render(request, 't2/color_easy.html')
+
+def color_hard(request):
+    return render(request, 't2/color_hard.html')
+
+def info(request):
+    return render(request, 't2/info.html')
