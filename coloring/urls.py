ea86467a008ee34954a3bee5946998c1a50280e4
Claire Yang <claire.yang@berkeley.edussh>
2021-07-13 14:03:37 -0400

    fixed url navigation

--- coloring/urls.py
+++ coloring/urls.py
@@ -4,10 +4,11 @@ from . import views
 urlpatterns = [
     path('demo', views.index, name='demo'),
     path('new_interaction', views.index, name='new_interaction'),
-    path('color_easy/', views.color_easy, name='color_easy'),
-    path('color_hard/', views.color_hard, name='color_hard'),
-    path('rec_easy/', views.rec_easy, name='rec_easy'),
-    path('rec_hard/', views.rec_hard, name='rec_hard'),
-    path('info/', views.info, name='info')
-    
+    path('home', views.home, name='home'),
+    path('color_easy', views.color_easy, name='color_easy'),
+    path('color_hard', views.color_hard, name='color_hard'),
+    path('rec_easy', views.rec_easy, name='rec_easy'),
+    path('rec_hard', views.rec_hard, name='rec_hard'),
+    path('info', views.info, name='info')
+
 ]
--- coloring/views.py
+++ coloring/views.py
@@ -1,19 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 def index(request):
     return render(request, 'coloring/index.html')
 
+def home(request):
+    return render(request, 'coloring/home.html')
+
 def rec_easy(request):
-    return render(request, 't2/rec_easy.html')
+    return render(request, 'coloring/rec_easy.html')
 
 def rec_hard(request):
-    return render(request, 't2/rec_hard.html')
+    return render(request, 'coloring/rec_hard.html')
 
 def color_easy(request):
-    return render(request, 't2/color_easy.html')
+    return render(request, 'coloring/color_easy.html')
 
 def color_hard(request):
-    return render(request, 't2/color_hard.html')
+    return render(request, 'coloring/color_hard.html')
 
 def info(request):
-    return render(request, 't2/info.html')
+    return render(request, 'coloring/info.html')
